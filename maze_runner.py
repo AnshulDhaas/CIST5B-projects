39e087589a3f0ca76ab23bab758d304ffb67fb5a
AnshulDhaas <anshuldhaas@gmail.com>
2024-09-10 20:02:41 -0700

    first commit

---
+++ maze_runner.py
@@ -0,0 +1,12 @@
+def maze_runner(maze):
+    START = (0, 0)
+    EXIT = (len(maze) - 1, len(maze[0]) - 1)
+    
+    def is_valid(pos):
+        
+    #base case: if we reach the exit
+    #recursive case: if the position is valid
+    
+    #mark the index
+    
+    #if we hit a wall/no path is found, backtrack and change the cell back to 0
