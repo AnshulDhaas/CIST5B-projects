f1bd0a530a64dedc7fb05f4c59ae0f2de123ae17
AnshulDhaas <anshuldhaas@gmail.com>
2024-09-10 20:07:50 -0700

    test

--- maze_runner.py
+++ maze_runner.py
@@ -2,9 +2,18 @@ def maze_runner(maze):
     START = (0, 0)
     EXIT = (len(maze) - 1, len(maze[0]) - 1)
     
-    def is_valid(pos):
+    def is_valid(x, y):
         
-    #base case: if we reach the exit
+    def navigate(x, y):
+        #base case: if we reach the exit
+        if (x, y) == EXIT:
+            maze[x][y] = 2
+            return True
+        
+        if not is_valid(x, y):
+            return False
+        
+        for 
     #recursive case: if the position is valid
     
     #mark the index
